dfe078bff36cbabcc032c4ef68adfdd4a28e4734
kimhansol <ksot1233@gmail.com>
2025-02-19 17:08:24 +0900

    seed 추가

---
+++ prisma/seed.js
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+import { PRODUCTS, ARTICLES } from "./mock.js";
+
+const prisma = PrismaClient();
+
+async function main() {
+  await prisma.product.deleteMany();
+  await prisma.article.deleteMany();
+}
+
+await prisma.product.createMany({
+  data: PRODUCTS,
+  skipDuplicates: true,
+});
+
+await prisma.article.createMany({
+  data: ARTICLES,
+  skipDuplicates: true,
+});
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.log(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
