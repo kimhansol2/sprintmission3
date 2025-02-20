d745585eced63c26ee4a930b4847cd6f9785ae68
kimhansol <ksot1233@gmail.com>
2025-02-20 11:04:17 +0900

    product CRUD 완료

--- src/routes/product.js
+++ src/routes/product.js
@@ -6,29 +6,76 @@ import { CreateProduct, PatchProduct } from "../structs.js";
 const prisma = new PrismaClient();
 const productRouter = express.Router();
 
-productRouter.route("/").post(async (req, res) => {
-  assert(req.body, CreateProduct);
-  const product = await prisma.product.create({ data: req.body });
+productRouter
+  .route("/")
+  .post(async (req, res) => {
+    assert(req.body, CreateProduct);
+    const product = await prisma.product.create({ data: req.body });
 
-  res.status(201).json({ message: product });
-});
+    res.status(201).json({ message: product });
+  })
+  .get(async (req, res) => {
+    const { offset, limit, create = "recent", name, description } = req.query;
+    let orderBy;
+    switch (create) {
+      case "old":
+        orderBy = { createdAt: "asc" };
+        break;
+      case "recent":
+        orderBy = { createdAt: "desc" };
+        break;
+      default:
+        orderBy = { createdAt: "desc" };
+    }
+    const where = {};
+    if (name) {
+      const nameTrimmed = name.replace(/\s+/g, " ").trim();
+      where.name = { contains: nameTrimmed, mode: "insensitive" };
+    }
+    if (description) {
+      const descriptionTrimmed = description.replace(/\s+/g, " ").trim();
+      where.description = { contains: descriptionTrimmed, mode: "insensitive" };
+    }
+
+    const product = await prisma.product.findMany({
+      where,
+      orderBy,
+      skip: parseInt(offset) || 0,
+      take: parseInt(limit) || 10,
+      select: {
+        id: true,
+        name: true,
+        price: true,
+        createdAt: true,
+      },
+    });
+    res.send(product);
+  });
 
 productRouter
   .route("/:id")
   .get(async (req, res) => {
     const { id } = req.params;
     const product = await prisma.product.findUniqueOrThrow({
-      where: id,
+      where: { id },
     });
-    res.json({ message: product });
+    res.send(product);
   })
   .patch(async (req, res) => {
     const { id } = req.params;
     assert(req.body, PatchProduct);
-    const product = prisma.product.update({
-      where: id,
+    const product = await prisma.product.update({
+      where: { id },
       data: req.body,
     });
+    res.send(product);
+  })
+  .delete(async (req, res) => {
+    const { id } = req.params;
+    const product = await prisma.product.delete({
+      where: { id },
+    });
+    res.send(product);
   });
 
 export default productRouter;
