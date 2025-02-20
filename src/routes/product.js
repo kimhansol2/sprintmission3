e77242a8bf9b3d95d8bdbb3cb6843dfa45cb9fec
kimhansol <ksot1233@gmail.com>
2025-02-20 09:24:16 +0900

    product_get_patch

--- src/routes/product.js
+++ src/routes/product.js
@@ -7,9 +7,28 @@ const prisma = new PrismaClient();
 const productRouter = express.Router();
 
 productRouter.route("/").post(async (req, res) => {
+  assert(req.body, CreateProduct);
   const product = await prisma.product.create({ data: req.body });
 
   res.status(201).json({ message: product });
 });
+
+productRouter
+  .route("/:id")
+  .get(async (req, res) => {
+    const { id } = req.params;
+    const product = await prisma.product.findUniqueOrThrow({
+      where: id,
+    });
+    res.json({ message: product });
+  })
+  .patch(async (req, res) => {
+    const { id } = req.params;
+    assert(req.body, PatchProduct);
+    const product = prisma.product.update({
+      where: id,
+      data: req.body,
+    });
+  });
 
 export default productRouter;
